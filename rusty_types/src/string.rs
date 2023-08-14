4df9c65654b78ab65b9d4afa8618b3c9f86a5c37
Elijah Mooring <vehmloewff@gmail.com>
2023-08-14 16:03:15 -0700

    ref: array and string don't need ownership

--- rusty_types/src/string.rs
+++ rusty_types/src/string.rs
@@ -1,20 +1,16 @@
 pub struct Str(String);
 
 impl Str {
-    pub fn new(string: &String) -> Str {
-        Str(string.clone())
+    pub fn new(string: &str) -> Str {
+        Str(String::from(string))
     }
 
-    pub fn concat(&self, other: &Str) -> Str {
+    pub fn concat(&self, other: Str) -> Str {
         let mut a = self.0.clone();
         let b = &other.0;
 
         a.push_str(b);
 
         Str(a)
-    }
-
-    pub fn get_length(&self) -> Number {
-        Number::from_usize(self.0.len())
     }
 }
